f3add90831c16af0c6c8adb6495b9fc64a75da5f
xiaoxiaoikhoudvan <40821790+xiaoxiaoikhoudvan@users.noreply.github.com>
2022-05-09 22:44:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`访问成功`)
+    response.write(`修改第一版本`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
